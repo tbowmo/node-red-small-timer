d336d254a7a5dc787c56242afce472e352017905
Thomas Bowman Mørch <thomas.git@bowmo.dk>
2023-09-28 20:33:23 +0200

    reset property did not correctly reset the node

--- src/lib/small-timer-runner.ts
+++ src/lib/small-timer-runner.ts
@@ -305,8 +305,11 @@ export class SmallTimerRunner {
         const payload = typeof incomingMsg.payload === 'string'
             ? incomingMsg.payload.toLocaleLowerCase()
             : incomingMsg.payload
+
         if (incomingMsg.reset) {
-            this.doOverride('auto') 
+            this.doOverride('auto')
+            this.forceSend()
+            return 
         }
 
         const timeout = incomingMsg.timeout !== undefined ? Number(incomingMsg.timeout) : undefined
